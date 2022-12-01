484e39aea03e618ddb2c50a4cc69ec6941d98eb0
Louis <louis@vulpem.com>
2022-12-01 12:26:21 +0100

    add websocat-installer step fetching websocat binaries

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#/!bin/bash
+#!/bin/bash
 set -m
 electrs&
 websocat -b ws-l:127.0.0.1:1234 tcp:127.0.0.1:50001&
---
+++ install-websocat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/vi/websocat/releases/download/v1.11.0/websocat.x86_64-unknown-linux-musl
+chmod +x websocat.x86_64-unknown-linux-musl
+mv websocat.x86_64-unknown-linux-musl /usr/local/bin/websocat
