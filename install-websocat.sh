546ad55f42501a55664046099723759b9239dccd
Louis <louis@vulpem.com>
2022-12-05 15:01:40 +0100

    inline install-websocat.sh

--- install-websocat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://github.com/vi/websocat/releases/download/v1.11.0/websocat.x86_64-unknown-linux-musl
-chmod +x websocat.x86_64-unknown-linux-musl
-mv websocat.x86_64-unknown-linux-musl /usr/local/bin/websocat
