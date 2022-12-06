12228c2794ce93248fbd0d14d0a317f4e79860f3
Louis <louis@vulpem.com>
2022-12-06 13:06:17 +0100

    PORT argument in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+PORT=$1
+
+if [ -z "$PORT" ]; then
+    echo "Usage: $0 <WS_PORT>"
+    exit 1
+fi
+
+echo "Starting websocket server on port $PORT"
+
 set -m
 electrs&
-websocat -b ws-l:127.0.0.1:1234 tcp:127.0.0.1:50001&
+websocat -b ws-l:127.0.0.1:$PORT tcp:127.0.0.1:50001&
 fg %1
