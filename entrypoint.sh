2200ba1a5e9d89cd14553a4af85a28d7b8fd822a
Louis <louis@vulpem.com>
2022-12-07 23:49:37 +0100

    add ELECTRUM_PORT env parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 PORT=$1
+ELECTRUM_RPC_PORT=$2
 
-if [ -z "$PORT" ]; then
-    echo "Usage: $0 <WS_PORT>"
+if [ -z "$PORT" ] || [ -z "$ELECTRUM_RPC_PORT" ]; then
+    echo "Usage: $0 <WS_PORT> <ELECTRUM_RPC_PORT>"
     exit 1
 fi
 
@@ -11,5 +12,5 @@ echo "Starting websocket server on port $PORT"
 
 set -m
 electrs&
-websocat -b ws-l:127.0.0.1:$PORT tcp:127.0.0.1:50001&
+websocat -b ws-l:127.0.0.1:$PORT tcp:127.0.0.1:$ELECTRUM_RPC_PORT&
 fg %1
